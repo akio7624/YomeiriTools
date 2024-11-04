d38966267937cf865b386b5b131fbe6ef4cbbe22
akio7624 <97262588+akio7624@users.noreply.github.com>
2024-11-04 14:29:28 +0900

    add apk.PACKHEDR

--- parser/apk.py
+++ parser/apk.py
@@ -1,4 +1,5 @@
 from datatype.chararray import chararray
+from datatype.uint32 import uint32
 from datatype.uint64 import uint64
 
 
@@ -6,14 +7,15 @@ class APK:
 
     def __init__(self):
         self.ENDIANNESS = self.__ENDIANNESS()
+        self.PACKHEDR = self.__PACKHEDR()
 
     class __ENDIANNESS:
         def __init__(self):
             self.SIGNATURE: chararray = chararray(size=8)
             self.TABLE_SIZE: uint64 = uint64(0)
 
-            self.SIGNATURE_ofs = 0
-            self.TABLE_SIZE_ofs = 0
+            self.SIGNATURE_ofs: int = 0
+            self.TABLE_SIZE_ofs: int = 0
 
         def from_bytearray(self, ofs: int, src: bytearray):
             if len(src) != 16:
@@ -22,7 +24,7 @@ class APK:
             self.SIGNATURE.from_bytearray(src[:8])
             self.SIGNATURE_ofs = ofs
             if str(self.SIGNATURE) != "ENDILTLE":
-                raise TableException(self, f"ENDIANNESS must be 'ENDILTLE'.  this={str(self.SIGNATURE)}")
+                raise TableException(self, f"SIGNATURE must be 'ENDILTLE'.  this={str(self.SIGNATURE)}")
 
             self.TABLE_SIZE.from_bytearray(src[8:])
             self.TABLE_SIZE_ofs = ofs + 8
@@ -30,8 +32,57 @@ class APK:
                 raise TableException(self, f"TABLE_SIZE must be 0.  this={int(self.TABLE_SIZE)}")
 
         def to_bytearray(self) -> bytearray:
-            # TODO
-            pass
+            return self.SIGNATURE.to_bytearray() + self.TABLE_SIZE.to_bytearray()
+
+    class __PACKHEDR:
+        def __init__(self):
+            self.SIGNATURE: chararray = chararray(size=8)
+            self.TABLE_SIZE: uint64 = uint64(0)
+            self.unknown_1: bytearray = bytearray()
+            self.FILE_LIST_OFFSET: uint32 = uint32(0)
+            self.ARCHIVE_PADDING_TYPE: uint32 = uint32(0)
+            self.HASH: bytearray = bytearray()
+
+            self.SIGNATURE_ofs: int = 0
+            self.TABLE_SIZE_ofs: int = 0
+            self.unknown_1_ofs: int = 0
+            self.FILE_LIST_OFFSET_ofs: int = 0
+            self.ARCHIVE_PADDING_TYPE_ofs: int = 0
+            self.HASH_ofs: int = 0
+
+        def from_bytearray(self, ofs: int, src: bytearray):
+            if len(src) != 48:
+                raise TableException(self, f"The table size must be 16.  this={len(src)}")
+
+            self.SIGNATURE.from_bytearray(src[:8])
+            self.SIGNATURE_ofs = ofs
+            if str(self.SIGNATURE) != "PACKHEDR":
+                raise TableException(self, f"SIGNATURE must be 'PACKHEDR'.  this={str(self.SIGNATURE)}")
+
+            self.TABLE_SIZE.from_bytearray(src[8:16])
+            self.TABLE_SIZE_ofs = ofs + 8
+
+            self.unknown_1 = src[16:24]
+            self.unknown_1_ofs = ofs + 16
+
+            self.FILE_LIST_OFFSET.from_bytearray(src[24:28])
+            self.FILE_LIST_OFFSET_ofs = ofs + 24
+
+            self.ARCHIVE_PADDING_TYPE.from_bytearray(src[28:32])
+            self.ARCHIVE_PADDING_TYPE_ofs = ofs + 28
+
+            self.HASH = src[32:]
+            self.HASH_ofs = ofs + 32
+
+        def to_bytearray(self) -> bytearray:
+            return (
+                        self.SIGNATURE.to_bytearray() +
+                        self.TABLE_SIZE.to_bytearray() +
+                        self.unknown_1 +
+                        self.FILE_LIST_OFFSET.to_bytearray() +
+                        self.ARCHIVE_PADDING_TYPE.to_bytearray() +
+                        self.HASH
+                    )
 
 
 class TableException(Exception):
