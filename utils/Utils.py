7ea3002e7afefe332f320731144ecbc688a6c12a
akio7624 <97262588+akio7624@users.noreply.github.com>
2024-11-04 14:29:59 +0900

    make support utils.bytes2hex multiline

--- utils/Utils.py
+++ utils/Utils.py
@@ -1,6 +1,12 @@
 def bytes2hex(v: bytearray) -> str:
-    return " ".join(f"{b:02X}" for b in v).zfill(8)
+    result = ""
+
+    for i in range(0, len(v), 8):
+        result += " ".join(f"{b:02X}" for b in v[i:i+8]).zfill(8)
+        result += "\n"
+
+    return result.strip()
 
 
-def hexoffset(v: int, size: int = 8) -> str:
+def hexoffset(v: int) -> str:
     return f"0x{v:08X}"
