8e0f37a154a80fc293be963ddadb1cf499d0faea
akio7624 <97262588+akio7624@users.noreply.github.com>
2024-11-04 10:46:53 +0900

    add test_dump_apk

---
+++ tests/test_dump_apk.py
@@ -0,0 +1,31 @@
+import os.path
+import unittest
+
+from scripts.dump_apk import DumpApk
+
+
+class TestDumpApk(unittest.TestCase):
+    def setUp(self):
+        self.samples_path = os.path.join("..", "samples")
+        self.all_apk_path = os.path.join("..", "samples", "all.apk")
+        self.fs_apk_path = os.path.join("..", "samples", "fs.apk")
+
+    def test_dump_table_all_apk(self):
+        self.parser = DumpApk(self.all_apk_path, os.path.join(self.samples_path, "dump_result", "test__dump_apk__all.apk__.txt"), "table", True)
+        self.parser.dump()
+
+    def test_dump_json_all_apk(self):
+        self.parser = DumpApk(self.all_apk_path, os.path.join(self.samples_path, "dump_result", "test__dump_apk__all.apk__.json"), "json", True)
+        self.parser.dump()
+
+    def test_dump_table_fs_apk(self):
+        self.parser = DumpApk(self.all_apk_path, os.path.join(self.samples_path, "dump_result", "test__dump_apk__fs.apk__.txt"), "table", True)
+        self.parser.dump()
+
+    def test_dump_json_fs_apk(self):
+        self.parser = DumpApk(self.all_apk_path, os.path.join(self.samples_path, "dump_result", "test__dump_apk__fs.apk__.json"), "json", True)
+        self.parser.dump()
+
+
+if __name__ == '__main__':
+    unittest.main()
