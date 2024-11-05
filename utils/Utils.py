3123f7049d27679e7ab2ff9b0eb020d7339f8629
akio7624 <97262588+akio7624@users.noreply.github.com>
2024-11-05 10:27:55 +0900

    add get_table_padding_count()

--- utils/Utils.py
+++ utils/Utils.py
@@ -13,3 +13,26 @@ def bytes2hex(v: bytearray) -> str:
 
 def hexoffset(v: int) -> str:
     return f"0x{v:08X}"
+
+
+def get_table_padding_count(current: int) -> int:
+    if current % 16 == 0:
+        return 0
+
+    return 16 - (current % 16)
+
+
+def identifier2desc(v: int) -> str:
+    if v == 0:
+        return "raw file"
+    elif v == 1:
+        return "directory"
+    elif v == 512:
+        return "zlib compressed file"
+
+
+def archive_padding_type2desc(v: int) -> str:
+    if v == 1:
+        return "Padded archive to be divisible by 2048"
+    elif v == 2:
+        return "Padded archive to be divisible by 512"
