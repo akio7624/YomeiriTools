7cb4655d758a0cf1e2e81c40ea4817aaa61301ab
akio7624 <97262588+akio7624@users.noreply.github.com>
2024-11-05 10:29:29 +0900

    add PACKTOC and PACKFSLS

--- parser/apk.py
+++ parser/apk.py
@@ -1,6 +1,7 @@
 from datatype.chararray import chararray
 from datatype.uint32 import uint32
 from datatype.uint64 import uint64
+from utils.Utils import get_table_padding_count
 
 
 class APK:
@@ -8,6 +9,8 @@ class APK:
     def __init__(self):
         self.ENDIANNESS = self.__ENDIANNESS()
         self.PACKHEDR = self.__PACKHEDR()
+        self.PACKTOC = self.__PACKTOC()
+        self.PACKFSLS = self.__PACKFSLS()
 
     class __ENDIANNESS:
         def __init__(self):
@@ -51,9 +54,6 @@ class APK:
             self.HASH_ofs: int = 0
 
         def from_bytearray(self, ofs: int, src: bytearray):
-            if len(src) != 48:
-                raise TableException(self, f"The table size must be 16.  this={len(src)}")
-
             self.SIGNATURE.from_bytearray(src[:8])
             self.SIGNATURE_ofs = ofs
             if str(self.SIGNATURE) != "PACKHEDR":
@@ -61,6 +61,9 @@ class APK:
 
             self.TABLE_SIZE.from_bytearray(src[8:16])
             self.TABLE_SIZE_ofs = ofs + 8
+
+            if len(src) != int(self.TABLE_SIZE) + 16:
+                raise TableException(self, f"The table size mismatch.  this={len(src)} expected={int(self.TABLE_SIZE) + 16}")
 
             self.unknown_1 = src[16:24]
             self.unknown_1_ofs = ofs + 16
@@ -70,6 +73,8 @@ class APK:
 
             self.ARCHIVE_PADDING_TYPE.from_bytearray(src[28:32])
             self.ARCHIVE_PADDING_TYPE_ofs = ofs + 28
+            if int(self.ARCHIVE_PADDING_TYPE) not in [1, 2]:
+                raise TableException(self, f"The ARCHIVE PADDING TYPE must be 1 or 2.  this={int(self.ARCHIVE_PADDING_TYPE)}")
 
             self.HASH = src[32:]
             self.HASH_ofs = ofs + 32
@@ -83,6 +88,238 @@ class APK:
                         self.ARCHIVE_PADDING_TYPE.to_bytearray() +
                         self.HASH
                     )
+
+    class __PACKTOC:
+        def __init__(self):
+            self.SIGNATURE: chararray = chararray(size=8)
+            self.TABLE_SIZE: uint64 = uint64(0)
+            self.TOC_SEG_SIZE: uint32 = uint32(0)
+            self.TOC_SEG_COUNT: uint32 = uint32(0)
+            self.unknown_1: bytearray = bytearray()
+            self.TOC_SEGMENT_LIST: list["__PACKTOC.__TOC_SEGMENT"] = []
+            self.PADDING: bytearray = bytearray()
+
+            self.SIGNATURE_ofs: int = 0
+            self.TABLE_SIZE_ofs: int = 0
+            self.TOC_SEG_SIZE_ofs: int = 0
+            self.TOC_SEG_COUNT_ofs: int = 0
+            self.unknown_1_ofs: int = 0
+            self.TOC_SEGMENT_LIST_ofs: int = 0
+            self.PADDING_ofs: int = 0
+
+        def from_bytearray(self, ofs: int, src: bytearray):
+            self.SIGNATURE.from_bytearray(src[:8])
+            self.SIGNATURE_ofs = ofs
+            if str(self.SIGNATURE) != "PACKTOC ":
+                raise TableException(self, f"SIGNATURE must be 'PACKTOC'.  this='{str(self.SIGNATURE)}'")
+
+            self.TABLE_SIZE.from_bytearray(src[8:16])
+            self.TABLE_SIZE_ofs = ofs + 8
+            if len(src) != int(self.TABLE_SIZE) + 16:
+                raise TableException(self, f"The table size mismatch.  this={len(src)} expected={int(self.TABLE_SIZE) + 16}")
+
+            self.TOC_SEG_SIZE.from_bytearray(src[16:20])
+            self.TOC_SEG_SIZE_ofs = ofs + 16
+            if int(self.TOC_SEG_SIZE) != 40:
+                raise TableException(self, f"The TOC_SEG_SIZE mismatch.  this={int(self.TOC_SEG_SIZE)}, expected=40")
+
+            self.TOC_SEG_COUNT.from_bytearray(src[20:24])
+            self.TOC_SEG_COUNT_ofs = ofs + 20
+
+            self.unknown_1 = src[24:32]
+            self.unknown_1_ofs = ofs + 24
+
+            seg_ofs = 32
+            self.TOC_SEGMENT_LIST_ofs = ofs + 32
+            for i in range(int(self.TOC_SEG_COUNT)):
+                seg = self.__TOC_SEGMENT()
+                seg.from_bytearray(ofs=ofs + seg_ofs, src=src[seg_ofs:seg_ofs + int(self.TOC_SEG_SIZE)])
+                self.TOC_SEGMENT_LIST.append(seg)
+                seg_ofs += int(self.TOC_SEG_SIZE)
+
+            self.PADDING = src[seg_ofs:seg_ofs + get_table_padding_count(seg_ofs)]
+            self.PADDING_ofs = ofs + seg_ofs
+
+        def to_bytearray(self) -> bytearray:
+            part_A: bytearray = (
+                        self.SIGNATURE.to_bytearray() +
+                        self.TABLE_SIZE.to_bytearray() +
+                        self.TOC_SEG_SIZE.to_bytearray() +
+                        self.TOC_SEG_COUNT.to_bytearray() +
+                        self.unknown_1
+                    )
+
+            part_B = bytearray()
+            for seg in self.TOC_SEGMENT_LIST:
+                part_B += seg.to_bytearray()
+
+            part_C: bytearray = self.PADDING
+
+            return part_A + part_B + part_C
+
+        class __TOC_SEGMENT:
+            def __init__(self):
+                self.IDENTIFIER: uint32 = uint32(0)
+                self.NAME_IDX: uint32 = uint32(0)
+                self.ZERO: bytearray = bytearray()
+                self.FILE_OFFSET: uint64 = uint64(0)  # for file
+                self.ENTRY_INDEX: uint32 = uint32(0)  # for directory
+                self.ENTRY_COUNT: uint32 = uint32(0)  # for directory
+                self.FILE_SIZE: uint64 = uint64(0)
+                self.FILE_ZSIZE: uint64 = uint64(0)
+
+                self.IDENTIFIER_ofs: int = 0
+                self.NAME_IDX_ofs: int = 0
+                self.ZERO_ofs: int = 0
+                self.FILE_OFFSET_ofs: int = 0
+                self.ENTRY_INDEX_ofs: int = 0
+                self.ENTRY_COUNT_ofs: int = 0
+                self.FILE_SIZE_ofs: int = 0
+                self.FILE_ZSIZE_ofs: int = 0
+
+            def from_bytearray(self, ofs: int, src: bytearray):
+                self.IDENTIFIER.from_bytearray(src[:4])
+                self.IDENTIFIER_ofs = ofs
+                if int(self.IDENTIFIER) not in [0, 1, 512]:
+                    raise TableException(self, f"IDENTIFIER must be 0 or 1 or 512    this={int(self.IDENTIFIER)}")
+
+                self.NAME_IDX.from_bytearray(src[4:8])
+                self.NAME_IDX_ofs = ofs + 4
+
+                self.ZERO = src[8:16]
+                self.ZERO_ofs = ofs + 8
+
+                if int(self.IDENTIFIER) == 1:  # if directory
+                    self.ENTRY_INDEX.from_bytearray(src[16:20])
+                    self.ENTRY_INDEX_ofs = ofs + 16
+                    self.ENTRY_COUNT.from_bytearray(src[20:24])
+                    self.ENTRY_COUNT_ofs = ofs + 20
+                else:  # if file
+                    self.FILE_OFFSET.from_bytearray(src[16:24])
+                    self.FILE_OFFSET_ofs = ofs + 16
+
+                self.FILE_SIZE.from_bytearray(src[24:32])
+                self.FILE_SIZE_ofs = ofs + 24
+
+                self.FILE_ZSIZE.from_bytearray(src[32:40])
+                self.FILE_ZSIZE_ofs = ofs + 32
+
+            def to_bytearray(self) -> bytearray:
+                return (
+                    self.IDENTIFIER.to_bytearray() +
+                    self.NAME_IDX.to_bytearray() +
+                    self.ZERO +
+                    self.FILE_OFFSET.to_bytearray() +
+                    self.FILE_SIZE.to_bytearray() +
+                    self.FILE_ZSIZE.to_bytearray()
+                )
+
+    class __PACKFSLS:
+        def __init__(self):
+            self.SIGNATURE: chararray = chararray(size=8)
+            self.TABLE_SIZE: uint64 = uint64(0)
+            self.ARCHIVE_SEG_COUNT: uint32 = uint32(0)
+            self.ARCHIVE_SEG_SIZE: uint32 = uint32(0)
+            self.unknown_1: bytearray = bytearray()
+            self.ARCHIVE_SEGMENT_LIST: list["__PACKFSLS.__ARCHIVE_SEGMENT"] = []
+            self.PADDING: bytearray = bytearray()
+
+            self.SIGNATURE_ofs: int = 0
+            self.TABLE_SIZE_ofs: int = 0
+            self.ARCHIVE_SEG_COUNT_ofs: int = 0
+            self.ARCHIVE_SEG_SIZE_ofs: int = 0
+            self.unknown_1_ofs: int = 0
+            self.ARCHIVE_SEGMENT_LIST_ofs: int = 0
+            self.PADDING_ofs: int = 0
+
+        def from_bytearray(self, ofs: int, src: bytearray):
+            self.SIGNATURE.from_bytearray(src[:8])
+            self.SIGNATURE_ofs = ofs
+            if str(self.SIGNATURE) != "PACKFSLS":
+                raise TableException(self, f"SIGNATURE must be 'PACKFSLS'.  this='{str(self.SIGNATURE)}'")
+
+            self.TABLE_SIZE.from_bytearray(src[8:16])
+            self.TABLE_SIZE_ofs = ofs + 8
+            if len(src) != int(self.TABLE_SIZE) + 16:
+                raise TableException(self, f"The table size mismatch.  this={len(src)} expected={int(self.TABLE_SIZE) + 16}")
+
+            self.ARCHIVE_SEG_COUNT.from_bytearray(src[16:20])
+            self.ARCHIVE_SEG_COUNT_ofs = ofs + 16
+
+            self.ARCHIVE_SEG_SIZE.from_bytearray(src[20:24])
+            self.ARCHIVE_SEG_SIZE_ofs = ofs + 20
+            if int(self.ARCHIVE_SEG_SIZE) != 40:
+                raise TableException(self, f"The ARCHIVE_SEG_SIZE mismatch.  this={int(self.ARCHIVE_SEG_SIZE)}, expected=40")
+
+            self.unknown_1 = src[24:32]
+            self.unknown_1_ofs = ofs + 24
+
+            seg_ofs = 32  # TODO
+            self.ARCHIVE_SEGMENT_LIST_ofs = ofs + 32
+            for i in range(int(self.ARCHIVE_SEG_COUNT)):
+                seg = self.__ARCHIVE_SEGMENT()
+                seg.from_bytearray(ofs=ofs + seg_ofs, src=src[seg_ofs:seg_ofs + int(self.ARCHIVE_SEG_SIZE)])
+                self.ARCHIVE_SEGMENT_LIST.append(seg)
+                seg_ofs += int(self.ARCHIVE_SEG_SIZE)
+
+            self.PADDING = src[seg_ofs:seg_ofs + get_table_padding_count(seg_ofs)]
+            self.PADDING_ofs = ofs + seg_ofs
+
+        def to_bytearray(self) -> bytearray:
+            part_A: bytearray = (
+                        self.SIGNATURE.to_bytearray() +
+                        self.TABLE_SIZE.to_bytearray() +
+                        self.TOC_SEG_SIZE.to_bytearray() +
+                        self.TOC_SEG_COUNT.to_bytearray() +
+                        self.unknown_1
+                    )
+
+            part_B = bytearray()
+            for seg in self.TOC_SEGMENT_LIST:
+                part_B += seg.to_bytearray()
+
+            part_C: bytearray = self.PADDING
+
+            return part_A + part_B + part_C
+
+        class __ARCHIVE_SEGMENT:
+            def __init__(self):
+                self.NAME_IDX: uint32 = uint32(0)
+                self.ZERO: bytearray = bytearray()
+                self.ARCHIVE_OFFSET: uint64 = uint64(0)
+                self.ARCHIVE_SIZE: uint64 = uint64(0)
+                self.HASH: bytearray = bytearray()
+
+                self.NAME_IDX_ofs: int = 0
+                self.ZERO_ofs: int = 0
+                self.ARCHIVE_OFFSET_ofs: int = 0
+                self.ARCHIVE_SIZE_ofs: int = 0
+                self.HASH_ofs: int = 0
+
+            def from_bytearray(self, ofs: int, src: bytearray):
+                self.NAME_IDX.from_bytearray(src[:4])
+                self.NAME_IDX_ofs = ofs
+
+                self.ZERO = src[4:8]
+                self.ZERO_ofs = ofs + 4
+
+                self.ARCHIVE_OFFSET.from_bytearray(src[8:16])
+                self.ARCHIVE_OFFSET_ofs = ofs + 8
+
+                self.ARCHIVE_SIZE.from_bytearray(src[16:24])
+                self.ARCHIVE_SIZE_ofs = ofs + 16
+
+                self.HASH = src[24:40]
+                self.HASH_ofs = ofs + 24
+
+            def to_bytearray(self) -> bytearray:
+                return (
+                    self.NAME_IDX.to_bytearray() +
+                    self.ZERO +
+                    self.ARCHIVE_OFFSET.to_bytearray() +
+                    self.ARCHIVE_SIZE.to_bytearray() +
+                    self.HASH
+                )
 
 
 class TableException(Exception):
