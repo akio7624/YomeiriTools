b1d0e5330a705579a5fd3a54c83a9fff3dae5b6a
akio7624 <97262588+akio7624@users.noreply.github.com>
2024-11-14 18:14:49 +0900

    add test_is_dump_same_with_original

--- tests/test_dump_apk.py
+++ tests/test_dump_apk.py
@@ -1,3 +1,4 @@
+import hashlib
 import os.path
 import unittest
 
@@ -14,6 +15,24 @@ class TestDumpApk(unittest.TestCase):
     def test_dump_table_all_apk(self):
         self.parser = DumpApk(self.all_apk_path, os.path.join(self.samples_path, "dump_result", "test__dump_apk__all.apk__.txt"), "table", True)
         self.parser.dump()
+
+    def test_is_dump_same_with_original(self):
+        self.parser = DumpApk(self.all_apk_path, os.path.join(self.samples_path, "dump_result", "test__dump_apk__all.apk__.txt"), "table", True)
+        self.parser.dump(debug_no_dump=True)
+
+        with open(self.all_apk_path, "rb") as f:
+            data = f.read()
+            original_md5 = hashlib.md5(data).hexdigest()
+            original_size = len(data)
+
+        with open("../samples/temp.apk", "wb") as f:
+            f.write(self.parser.APK.to_bytearray())
+
+        dumped_md5 = hashlib.md5(self.parser.APK.to_bytearray()).hexdigest()
+        dumped_size = len(self.parser.APK.to_bytearray())
+
+        self.assertEqual(original_size, dumped_size)
+        self.assertEqual(original_md5, dumped_md5)
 
     def test_dump_table_fs_apk(self):
         self.parser = DumpApk(self.fs_apk_path, os.path.join(self.samples_path, "dump_result", "test__dump_apk__fs.apk__.txt"), "table", True)
