ada26908f1f32073bfea841fe29353e85c3b7784
akio7624 <97262588+akio7624@users.noreply.github.com>
2024-11-15 16:01:10 +0900

    create test_unpack_apk.py

---
+++ tests/test_unpack_apk.py
@@ -0,0 +1,27 @@
+import os.path
+import unittest
+
+from scripts.unpack_apk import UnpackApk
+
+
+class TestDumpApk(unittest.TestCase):
+    def setUp(self):
+        self.samples_path = os.path.join("..", "samples")
+        self.extract_path = os.path.join("..", "samples", "extract")
+        self.all_apk_path = os.path.join("..", "samples", "all.apk")
+        self.fs_apk_path = os.path.join("..", "samples", "fs.apk")
+        self.stage_apk_path = os.path.join("..", "samples", "stage.apk")
+
+    def test_all_apk(self):
+        outpath = os.path.join(self.extract_path, "all")
+        os.makedirs(outpath, exist_ok=True)
+        UnpackApk(self.all_apk_path, outpath, "overwrite").extract()
+
+    def test_fs_apk(self):
+        outpath = os.path.join(self.extract_path, "fs")
+        os.makedirs(outpath, exist_ok=True)
+        UnpackApk(self.fs_apk_path, outpath, "overwrite").extract()
+
+
+if __name__ == '__main__':
+    unittest.main()
