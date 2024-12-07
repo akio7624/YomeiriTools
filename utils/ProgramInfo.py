c25f08897c5d697b9548474572d6e4e3842bfddd
akio7624 <97262588+akio7624@users.noreply.github.com>
2024-12-07 20:47:24 +0900

    update version in ProgramInfo

--- utils/ProgramInfo.py
+++ utils/ProgramInfo.py
@@ -2,5 +2,5 @@ from datetime import datetime
 from typing import Final
 
 TOOL_NAME: Final[str] = "ymtools"
-TOOL_VERSION: Final[str] = "1.0.0"
-TOOL_UPDATE_DATE: Final[datetime] = datetime(2024, 11, 7)
+TOOL_VERSION: Final[str] = "2.0.0"
+TOOL_UPDATE_DATE: Final[datetime] = datetime(2024, 12, 8)
